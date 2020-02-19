c44e5c56c60fb7b81c74fba56af6de55f88e0af4
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-02-19 10:56:33 +0000

    3540: Add sample web api project

---
+++ Backend/samples/NHSD.BuyingCatalogue.Identity.Api.SampleResource/Controllers/IdentityController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NHSD.BuyingCatalogue.Identity.Api.SampleResource.Controllers
+{
+    [Route("identity")]
+    [Authorize]
+    public sealed class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
---
+++ Backend/samples/NHSD.BuyingCatalogue.Identity.Api.SampleResource/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NHSD.BuyingCatalogue.Identity.Api.SampleResource
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "http://identity_api:80";
+                    options.RequireHttpsMetadata = false;
+                    options.Audience = "SampleResource";
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
