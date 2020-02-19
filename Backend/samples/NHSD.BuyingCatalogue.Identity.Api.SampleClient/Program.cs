4ef731f3f61a6e38e650bb7c3e90035910afce44
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-02-19 10:49:07 +0000

    3540: Add sample client project

---
+++ Backend/samples/NHSD.BuyingCatalogue.Identity.Api.SampleClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace NHSD.BuyingCatalogue.Identity.Api.SampleClient
+{
+    public sealed class Program
+    {
+        private static async Task Main()
+        {
+            // discover endpoints from metadata
+            var client = new HttpClient();
+
+            var discoveryDocument = await client.GetDiscoveryDocumentAsync("http://localhost:8070");
+            if (discoveryDocument.IsError)
+            {
+                Console.WriteLine(discoveryDocument.Error);
+                return;
+            }
+            
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = discoveryDocument.TokenEndpoint,
+                ClientId = "SampleClient",
+                ClientSecret = "SampleClient",
+
+                Scope = "SampleResource"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("http://localhost:8071/Identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
