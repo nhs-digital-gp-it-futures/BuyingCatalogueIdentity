73d8f14da8bb3116ec56469bdf5a5d4f7d6ec50f
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-02-19 10:43:03 +0000

    3540: Add Identity Web Api project

---
+++ Backend/src/NHSD.BuyingCatalogue.Identity.Api/Startup.cs
@@ -0,0 +1,57 @@
+﻿using IdentityServer4.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NHSD.BuyingCatalogue.Identity.Api
+{
+    public sealed class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // uncomment, if you want to add an MVC-based UI
+            //services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer(options => options.IssuerUri = "http://localhost:8070")
+                .AddInMemoryIdentityResources(Config.Ids)
+                .AddInMemoryApiResources(Config.Apis)
+                .AddInMemoryClients(Config.Clients);
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseSwagger().UseSwaggerUI(options =>
+                //{
+                //    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Buying Catalogue Identity Service V1");
+                //});
+            }
+
+            // uncomment if you want to add MVC
+            //app.UseStaticFiles();
+            //app.UseRouting();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC
+            //app.UseAuthorization();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapDefaultControllerRoute();
+            //});
+        }
+    }
+}
