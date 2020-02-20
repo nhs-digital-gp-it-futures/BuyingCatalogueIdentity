251c8739025fed6d14b9c9bfedada1246ae9960b
Ruth Berry <ruth.berry@bjss.com>
2020-02-20 12:03:42 +0000

    3540: Login MVC Client initial commit

---
+++ Backend/samples/NHSD.BuyingCatalogue.Identity.Api.SampleLoginClient/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NHSD.BuyingCatalogue.Identity.Api.SampleLoginClient.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index(string redirectUrl)
+        {
+            ViewBag.RedirectUrl = redirectUrl;
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string password, string redirectUrl)
+        {
+            //create http client
+            using var client = new HttpClient();
+
+
+
+
+            return View("Index");
+        }
+    }
+}
---
+++ Backend/samples/NHSD.BuyingCatalogue.Identity.Api.SampleMvcClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NHSD.BuyingCatalogue.Identity.Api.SampleMvcClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
