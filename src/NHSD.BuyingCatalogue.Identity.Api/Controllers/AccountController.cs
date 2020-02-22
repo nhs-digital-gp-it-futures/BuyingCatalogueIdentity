e11ae23d80b42e6037d102c029e6c74ce82b5f3f
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-02-22 11:50:57 +0000

    5166: Switch to account controller

---
+++ src/NHSD.BuyingCatalogue.Identity.Api/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NHSD.BuyingCatalogue.Identity.Api.Infrastructure;
+using NHSD.BuyingCatalogue.Identity.Api.ViewModels;
+
+namespace NHSD.BuyingCatalogue.Identity.Api.Controllers
+{
+    public sealed class AccountController : Controller
+    {
+        private readonly IIdentityServerInteractionService _interaction;
+        
+        public AccountController(
+            IIdentityServerInteractionService interaction)
+        {
+            _interaction = interaction;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            LoginViewModel loginViewModel = new LoginViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            ViewData["ReturnUrl"] = returnUrl;
+
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel viewModel)
+        {
+            viewModel.ThrowIfNull();
+
+            var user = TestUsers.Users
+                .FirstOrDefault(usr => usr.Password == viewModel.Password && usr.Username == viewModel.Username);
+
+            if (user is null)
+            {
+                return Unauthorized();
+            }
+
+            await HttpContext.SignInAsync(user.SubjectId, user.Username);
+
+            // make sure the returnUrl is still valid, and if yes - redirect back to authorize endpoint
+            if (_interaction.IsValidReturnUrl(viewModel.ReturnUrl))
+            {
+                return Redirect(viewModel.ReturnUrl);
+            }
+
+            return Redirect("~/");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Error(string errorId)
+        {
+            // retrieve error details from identityserver
+            var message = await _interaction.GetErrorContextAsync(errorId);
+            
+            return Ok(message);
+        }
+    }
+}
