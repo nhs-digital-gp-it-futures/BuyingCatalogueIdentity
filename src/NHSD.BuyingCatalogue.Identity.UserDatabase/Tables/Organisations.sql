29a65f89023383f2cc6ec0ecb5763fa19c21a102
Ruth Berry <ruth.berry@bjss.com>
2020-03-12 14:51:06 +0000

    5146: Removed Default date setter

--- src/NHSD.BuyingCatalogue.Identity.UserDatabase/Tables/Organisations.sql
+++ src/NHSD.BuyingCatalogue.Identity.UserDatabase/Tables/Organisations.sql
@@ -4,7 +4,7 @@
     [Name] NVARCHAR(255) NOT NULL,    
     [Address] NVARCHAR(255) NULL,
     OdsCode NVARCHAR(8) NULL,
-    LastUpdated DATETIME2(7) NOT NULL DEFAULT GETUTCDATE(),
+    LastUpdated DATETIME2(7) NOT NULL,
     CONSTRAINT PK_Organisations PRIMARY KEY NONCLUSTERED (Id),
     INDEX IX_OrganisationName CLUSTERED ([Name])
 );
