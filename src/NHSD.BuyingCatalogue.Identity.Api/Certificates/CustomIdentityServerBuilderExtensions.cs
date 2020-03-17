2b2950aa7f0e787d5a2259d4e9517895aa372043
stuart-bass-bjss <59572183+stuart-bass-bjss@users.noreply.github.com>
2020-03-17 08:52:51 +0000

    disable ca2000, run code formatter as per review.

--- src/NHSD.BuyingCatalogue.Identity.Api/Certificates/CustomIdentityServerBuilderExtensions.cs
+++ src/NHSD.BuyingCatalogue.Identity.Api/Certificates/CustomIdentityServerBuilderExtensions.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.DependencyInjection;
 using NHSD.BuyingCatalogue.Identity.Api.Infrastructure;
 using NHSD.BuyingCatalogue.Identity.Api.Settings;
 using Serilog;
-using Serilog.Events;
 
 namespace NHSD.BuyingCatalogue.Identity.Api.Certificates
 {
@@ -21,17 +20,19 @@ namespace NHSD.BuyingCatalogue.Identity.Api.Certificates
                 logger.Information("Using Developer Signing Credential");
                 return builder.AddDeveloperSigningCredential();
             }
-        
+
             logger.Information("Using Certificate {certificatePath}", settings.CertificatePath);
             try
             {
+#pragma warning disable CA2000 // Dispose objects before losing scope - deliberately not disposing this certificate, as it is used by identity server later on.
                 var certificate = new X509Certificate2(settings.CertificatePath, settings.CertificatePassword);
+#pragma warning restore CA2000 // Dispose objects before losing scope
                 var verified = certificate.Verify();
-                if(verified)
+                if (verified)
                     logger.Information("Certificate Verified: {certificate}", certificate.ToString(true));
                 else
                     logger.Warning("Certificate Not Verified: {certificate}", certificate.ToString(true));
-                
+
                 return builder.AddSigningCredential(certificate);
             }
             catch (Exception e)
