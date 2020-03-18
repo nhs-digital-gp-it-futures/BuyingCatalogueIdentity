dcddec776abfd8a49f8fbd82aaf2006a519f9d5d
bjss-chris <christopher.martin@bjss.com>
2020-03-18 11:44:04 +0000

    Fix casing of organization names
    AB#5200

--- src/NHSD.BuyingCatalogue.Identity.UserDatabase/Deployment/CreateExecutiveAgency.sql
+++ src/NHSD.BuyingCatalogue.Identity.UserDatabase/Deployment/CreateExecutiveAgency.sql
@@ -3,5 +3,5 @@
 IF NOT EXISTS (SELECT * FROM dbo.Organisations WHERE PrimaryRoleId = @executiveAgencyRoleId)
 	INSERT INTO dbo.Organisations (OrganisationId, [Name], [Address], OdsCode, PrimaryRoleId)
 	VALUES
-	('C7A94E85-025B-403F-B984-20EE5F9EC333', 'NHS DIGITAL', '{"line1":"1 TREVELYAN SQUARE","town":"LEEDS","postcode":"LS1 6AE","country":"ENGLAND"}', 'X26', @executiveAgencyRoleId);
+	('C7A94E85-025B-403F-B984-20EE5F9EC333', 'NHS Digital', '{"line1":"1 TREVELYAN SQUARE","town":"LEEDS","postcode":"LS1 6AE","country":"ENGLAND"}', 'X26', @executiveAgencyRoleId);
 GO
