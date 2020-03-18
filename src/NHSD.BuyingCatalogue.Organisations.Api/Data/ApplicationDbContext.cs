1f74c34173b8e0795ed9acc4dcd50943a7d0f6ef
Ruth Berry <ruth.berry@bjss.com>
2020-03-18 08:13:34 +0000

    5147: Organisation code review change, appdbcontext

--- src/NHSD.BuyingCatalogue.Organisations.Api/Data/ApplicationDbContext.cs
+++ src/NHSD.BuyingCatalogue.Organisations.Api/Data/ApplicationDbContext.cs
@@ -18,7 +18,7 @@ namespace NHSD.BuyingCatalogue.Organisations.Api.Data
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Organisation>()
-                .Property(b => b.Address)
+                .Property(prop => prop.Address)
                 .HasConversion(
                     x => JsonConvert.SerializeObject(x, new JsonSerializerSettings(){NullValueHandling = NullValueHandling.Ignore}),
                     x => JsonConvert.DeserializeObject<Address>(x));
