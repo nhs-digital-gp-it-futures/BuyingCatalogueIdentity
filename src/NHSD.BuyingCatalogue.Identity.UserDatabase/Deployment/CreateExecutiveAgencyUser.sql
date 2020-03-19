94b0a5aa38fc63ebc10e0d35d944bd2db22b20f5
bjss-chris <christopher.martin@bjss.com>
2020-03-19 12:04:26 +0000

    Create executive agency user on deployment
    AB#4990

---
+++ src/NHSD.BuyingCatalogue.Identity.UserDatabase/Deployment/CreateExecutiveAgencyUser.sql
@@ -0,0 +1,25 @@
+﻿IF '$(CREATE_EA_USER)' = 'True'
+AND NOT EXISTS (
+    SELECT *
+      FROM dbo.AspNetUsers
+      WHERE UserName = '$(EA_USER_NAME)')
+BEGIN
+    DECLARE @normalizedUserName AS nvarchar(256) = UPPER('$(EA_USER_NAME)');
+
+    DECLARE @executiveAgencyRoleId AS nchar(5) = 'RO116';
+    DECLARE @organisationId AS uniqueidentifier = (SELECT OrganisationId FROM dbo.Organisations WHERE PrimaryRoleId = @executiveAgencyRoleId);
+
+    INSERT INTO dbo.AspNetUsers
+    (
+        Id, UserName, NormalizedUserName, PasswordHash, 
+        FirstName, LastName, Email, NormalizedEmail, EmailConfirmed, PhoneNumber, PhoneNumberConfirmed,
+        PrimaryOrganisationId, OrganisationFunction, CatalogueAgreementSigned, [Disabled],
+        AccessFailedCount, ConcurrencyStamp, LockoutEnabled, SecurityStamp, TwoFactorEnabled
+    )
+	VALUES
+	(CAST(NEWID() AS nchar(36)), '$(EA_USER_NAME)', @normalizedUserName, '$(EA_USER_PASSWORD_HASH)',
+        '$(EA_USER_FIRST_NAME)', '$(EA_USER_LAST_NAME)', '$(EA_USER_NAME)', @normalizedUserName, 1, '$(EA_USER_PHONE)', 1,
+        @organisationId, 'Authority', 1, 0,
+        0, NEWID(), 1, NEWID(), 0);
+END;
+GO
--- src/NHSD.BuyingCatalogue.Identity.UserDatabase/Deployment/PostDeployment.sql
+++ src/NHSD.BuyingCatalogue.Identity.UserDatabase/Deployment/PostDeployment.sql
@@ -12,4 +12,5 @@ Post-Deployment Script Template
 :r ./CreateClinicalCommissioningGroups.sql
 :r ./CreateCommissioningSupportUnits.sql
 :r ./CreateExecutiveAgency.sql
+:r ./CreateExecutiveAgencyUser.sql
 :r ./CreateTestUsers.sql
