3c4a4f83e17fa8bcc6dc52eca06693d4f2870c41
bjss-chris <christopher.martin@bjss.com>
2020-03-20 09:32:55 +0000

    Rename SQLCMD user name variable to email
    Renamed the EA_USER_NAME SQLCMD variable to EA_USER_EMAIL to make it to clear that it should be an e-mail address.
    
    AB#4990

--- src/NHSD.BuyingCatalogue.Identity.UserDatabase/Deployment/CreateExecutiveAgencyUser.sql
+++ src/NHSD.BuyingCatalogue.Identity.UserDatabase/Deployment/CreateExecutiveAgencyUser.sql
@@ -2,9 +2,9 @@
 AND NOT EXISTS (
     SELECT *
       FROM dbo.AspNetUsers
-      WHERE UserName = '$(EA_USER_NAME)')
+      WHERE UserName = '$(EA_USER_EMAIL)')
 BEGIN
-    DECLARE @normalizedUserName AS nvarchar(256) = UPPER('$(EA_USER_NAME)');
+    DECLARE @normalizedUserName AS nvarchar(256) = UPPER('$(EA_USER_EMAIL)');
 
     DECLARE @executiveAgencyRoleId AS nchar(5) = 'RO116';
     DECLARE @organisationId AS uniqueidentifier = (SELECT OrganisationId FROM dbo.Organisations WHERE PrimaryRoleId = @executiveAgencyRoleId);
@@ -17,8 +17,8 @@ BEGIN
         AccessFailedCount, ConcurrencyStamp, LockoutEnabled, SecurityStamp, TwoFactorEnabled
     )
 	VALUES
-	(CAST(NEWID() AS nchar(36)), '$(EA_USER_NAME)', @normalizedUserName, '$(EA_USER_PASSWORD_HASH)',
-        '$(EA_USER_FIRST_NAME)', '$(EA_USER_LAST_NAME)', '$(EA_USER_NAME)', @normalizedUserName, 1, '$(EA_USER_PHONE)', 1,
+	(CAST(NEWID() AS nchar(36)), '$(EA_USER_EMAIL)', @normalizedUserName, '$(EA_USER_PASSWORD_HASH)',
+        '$(EA_USER_FIRST_NAME)', '$(EA_USER_LAST_NAME)', '$(EA_USER_EMAIL)', @normalizedUserName, 1, '$(EA_USER_PHONE)', 1,
         @organisationId, 'Authority', 1, 0,
         0, NEWID(), 1, NEWID(), 0);
 END;
