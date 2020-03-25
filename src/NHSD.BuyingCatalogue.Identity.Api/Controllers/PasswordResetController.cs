4579895af4441aaae559bd54de00f8deb2aa57fc
Callum Bradbury <callum.bradbury@bjss.com>
2020-03-25 14:47:04 +0000

    3926: Initial change including LinkSent and ResetPassword pages

---
+++ src/NHSD.BuyingCatalogue.Identity.Api/Controllers/PasswordResetController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using NHSD.BuyingCatalogue.Identity.Api.Infrastructure;
+using NHSD.BuyingCatalogue.Identity.Api.ViewModels.PasswordReset;
+
+namespace NHSD.BuyingCatalogue.Identity.Api.Controllers
+{
+    [Route(@"Account")]
+    public class PasswordResetController : Controller
+    {
+        [HttpGet]
+        [Route(@"ResetPassword")]
+        public IActionResult ResetPassword(Uri returnUrl)
+        {
+            returnUrl.ThrowIfNull();
+            var viewModel = new PasswordResetViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route(@"ResetPassword")]
+        public IActionResult ResetPassword(PasswordResetViewModel viewModel)
+        {
+            viewModel.ThrowIfNull(nameof(viewModel));
+            viewModel.ReturnUrl.ThrowIfNull();
+            return RedirectToAction("LinkSent", new { returnUrl = viewModel.ReturnUrl });
+        }
+
+        [HttpGet]
+        [Route(@"LinkSent")]
+        public IActionResult LinkSent(Uri returnUrl)
+        {
+            returnUrl.ThrowIfNull();
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+    }
+}
---
+++ src/NHSD.BuyingCatalogue.Identity.Api/ViewModels/PasswordReset/PasswordResetViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace NHSD.BuyingCatalogue.Identity.Api.ViewModels.PasswordReset
+{
+    public sealed class PasswordResetViewModel
+    {
+        [Required(ErrorMessage = ErrorMessages.EmailAddressRequired)]
+        [EmailAddress(ErrorMessage = ErrorMessages.EmailAddressInvalid)]
+        [DisplayName("Email address")]
+        public string EmailAddress { get; set; }
+
+        public Uri ReturnUrl { get; set; }
+
+        internal static class ErrorMessages
+        {
+            internal const string EmailAddressRequired = "Enter your email address";
+            internal const string EmailAddressInvalid = "Enter a valid email address";
+        }
+    }
+}
