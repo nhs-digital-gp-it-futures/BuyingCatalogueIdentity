c014e84a2d03d8039a4ce00d62d89a019ade9f57
Ruth Berry <ruth.berry@bjss.com>
2020-03-27 13:36:54 +0000

    3926: Password reset pages

---
+++ src/NHSD.BuyingCatalogue.Identity.Api/Controllers/PasswordResetController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using NHSD.BuyingCatalogue.Identity.Api.Infrastructure;
+using NHSD.BuyingCatalogue.Identity.Api.ViewModels.PasswordReset;
+
+namespace NHSD.BuyingCatalogue.Identity.Api.Controllers
+{
+    [Route(@"Account")]
+    public class PasswordResetController : Controller
+    {
+        [HttpGet]
+        [Route(@"ResetPassword")]
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route(@"ResetPassword")]
+        public IActionResult ResetPassword(PasswordResetViewModel viewModel)
+        {
+            viewModel.ThrowIfNull(nameof(viewModel));
+            return RedirectToAction("ConfirmPasswordReset");
+        }
+
+        [HttpGet]
+        [Route(@"ConfirmPasswordReset")]
+        public IActionResult ConfirmPasswordReset()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/NHSD.BuyingCatalogue.Identity.Api/ViewModels/PasswordReset/PasswordResetViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NHSD.BuyingCatalogue.Identity.Api.ViewModels.PasswordReset
+{
+    public sealed class PasswordResetViewModel
+    {
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
