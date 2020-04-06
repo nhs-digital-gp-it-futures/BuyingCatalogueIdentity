0f589ea1a82bc72dffbd7e69c6ec5a38c8f78540
bjss-chris <christopher.martin@bjss.com>
2020-04-06 11:42:55 +0100

    Add unit tests for password reset token and app user
    AB#5615

---
+++ tests/NHSD.BuyingCatalogue.Identity.Api.UnitTests/Models/ApplicationUserTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NHSD.BuyingCatalogue.Identity.Api.Models;
+using NUnit.Framework;
+
+namespace NHSD.BuyingCatalogue.Identity.Api.UnitTests.Models
+{
+    [TestFixture]
+    internal sealed class ApplicationUserTests
+    {
+        [Test]
+        public void DisplayName_ReturnsExpectedValue()
+        {
+            var user = new ApplicationUser
+            {
+                FirstName = "Edgar",
+                LastName = "Poe",
+            };
+
+            user.DisplayName.Should().Be("Edgar Poe");
+        }
+    }
+}
