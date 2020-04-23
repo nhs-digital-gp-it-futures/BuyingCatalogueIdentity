dcf9f411662e2fbcd52e83d56dce229fdc92f398
Adrian Soomro <adrian.soomro@gmail.com>
2020-04-23 12:02:43 +0100

    6168: Return actual URL in 201 response Location header

---
+++ src/NHSD.BuyingCatalogue.Identity.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NHSD.BuyingCatalogue.Identity.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string TrimAsync(this string input)
+        {
+            if (input is null)
+                throw new ArgumentNullException(nameof(input));
+
+            return input.Replace("async", "", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ tests/NHSD.BuyingCatalogue.Identity.Common.UnitTests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using FluentAssertions;
+using NHSD.BuyingCatalogue.Identity.Common.Extensions;
+using NUnit.Framework;
+
+namespace NHSD.BuyingCatalogue.Identity.Common.UnitTests.Extensions
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    internal sealed class StringExtensionsTests
+    {
+
+        [Test]
+        public void TrimAsync__NullInput_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                string input = null;
+                input.TrimAsync();
+            });
+        }
+
+        [Test]
+        public void TrimAsync__InputContainingAsync_GetsTrimmed()
+        {
+            const string input = "FooAsync";
+            const string expected = "Foo";
+            string actual = input.TrimAsync();
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void TrimAsync__InputContainingAsync_GetsTrimmed_IgnoringCase()
+        {
+            const string input = "FooAsYnC";
+            const string expected = "Foo";
+            string actual = input.TrimAsync();
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void TrimAsync__InputNotContainingAsync_RemainsSame()
+        {
+            const string input = "Bar";
+            const string expected = input;
+            string actual = input.TrimAsync();
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
+
