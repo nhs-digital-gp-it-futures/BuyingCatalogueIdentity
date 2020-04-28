bbf8759d643506f2c27c06d6750acad8afab00af
bjss-chris <christopher.martin@bjss.com>
2020-04-28 13:37:04 +0100

    Delete unrequired using statements
    AB#6339

--- src/NHSD.BuyingCatalogue.Identity.Api/ViewModels/Account/ResetPasswordViewModel.cs
+++ src/NHSD.BuyingCatalogue.Identity.Api/ViewModels/Account/ResetPasswordViewModel.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 
 namespace NHSD.BuyingCatalogue.Identity.Api.ViewModels.Account
 {
