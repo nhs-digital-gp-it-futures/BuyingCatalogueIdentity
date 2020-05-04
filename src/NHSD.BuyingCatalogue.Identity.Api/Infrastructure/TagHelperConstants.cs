3c43dbef9c6d045b5d92856dfc7e4586d61f05b4
Ruth Berry <Ruth.berry@bjss.com>
2020-05-04 12:20:53 +0100

    6278: Tag Helper Constants, internal

--- src/NHSD.BuyingCatalogue.Identity.Api/Infrastructure/TagHelperConstants.cs
+++ src/NHSD.BuyingCatalogue.Identity.Api/Infrastructure/TagHelperConstants.cs
@@ -1,43 +1,43 @@
 ﻿namespace NHSD.BuyingCatalogue.Identity.Api.Infrastructure
 {
-    public static class TagHelperConstants
+    internal static class TagHelperConstants
     {
         // tags
-        public const string Div = "div";
-        public const string Anchor = "a";
-        public const string Link = "href";
-        public const string ListItem = "li";
-        public const string SubHeader = "h2";
-        public const string Span = "span";
-        public const string UnorderedList = "ul";
+        internal const string Div = "div";
+        internal const string Anchor = "a";
+        internal const string Link = "href";
+        internal const string ListItem = "li";
+        internal const string SubHeader = "h2";
+        internal const string Span = "span";
+        internal const string UnorderedList = "ul";
         internal const string Svg = "svg";
         internal const string Path = "path";
 
         // attributes
-        public const string DataTestId = "data-test-id";
-        public const string For = "asp-for";
-        public const string Id = "id";
-        public const string LabelledBy = "aria-labelledy";
-        public const string Role = "role";
-        public const string RoleAlert = "alert";
-        public const string Type = "type";
-        public const string Tabindex = "tabindex";
+        internal const string DataTestId = "data-test-id";
+        internal const string For = "asp-for";
+        internal const string Id = "id";
+        internal const string LabelledBy = "aria-labelledy";
+        internal const string Role = "role";
+        internal const string RoleAlert = "alert";
+        internal const string Type = "type";
+        internal const string Tabindex = "tabindex";
         internal const string Xmlns = "xmlns";
         internal const string ViewBox = "viewBox";
         internal const string AriaHidden = "aria-hidden";
 
         // classes
-        public const string ErrorSummaryTitle = "error-summary-title";
-        public const string NhsErrorMessage = "nhsuk-error-message";
-        public const string NhsFormGroup = "nhsuk-form-group";
-        public const string NhsFormGroupError = "nhsuk-form-group--error";
-        public const string NhsInput = "nhsuk-input";
-        public const string NhsLabel = "nhsuk-label";
-        public const string NhsList = "nhsuk-list";
-        public const string NhsValidationSummary = "nhsuk-error-summary";
-        public const string NhsValidationSummaryTitle = "nhsuk-error-summary__title";
-        public const string NhsValidationSummaryList = "nhsuk-error-summary__list";
-        public const string NhsValidationInputError = "nhsuk-input--error";
+        internal const string ErrorSummaryTitle = "error-summary-title";
+        internal const string NhsErrorMessage = "nhsuk-error-message";
+        internal const string NhsFormGroup = "nhsuk-form-group";
+        internal const string NhsFormGroupError = "nhsuk-form-group--error";
+        internal const string NhsInput = "nhsuk-input";
+        internal const string NhsLabel = "nhsuk-label";
+        internal const string NhsList = "nhsuk-list";
+        internal const string NhsValidationSummary = "nhsuk-error-summary";
+        internal const string NhsValidationSummaryTitle = "nhsuk-error-summary__title";
+        internal const string NhsValidationSummaryList = "nhsuk-error-summary__list";
+        internal const string NhsValidationInputError = "nhsuk-input--error";
         internal const string NhsBackLinkLink = "nhsuk-back-link__link";
         internal const string NhsIcon = "nhsuk-icon";
         internal const string NhsIconChevronLeft = "nhsuk-icon__chevron-left";
