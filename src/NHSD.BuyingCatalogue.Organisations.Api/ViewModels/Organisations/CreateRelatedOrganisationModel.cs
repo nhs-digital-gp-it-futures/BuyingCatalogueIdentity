951866b88a8c2bf1243942bfb89e09e52cc4e3f1
Lee Rogers <lee.rogers@bjss.com>
2021-04-09 14:42:20 +0100

    add createReleatedOrganisationModel and RelatedOrganisationModel so we can get and post information on the RelatedOrganisations

---
+++ src/NHSD.BuyingCatalogue.Organisations.Api/ViewModels/Organisations/CreateRelatedOrganisationModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NHSD.BuyingCatalogue.Organisations.Api.ViewModels.Organisations
+{
+    public class CreateRelatedOrganisationModel
+    {
+        public Guid RelatedOrganisationId { get; set; }
+    }
+}
---
+++ src/NHSD.BuyingCatalogue.Organisations.Api/ViewModels/Organisations/RelatedOrganisationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NHSD.BuyingCatalogue.Organisations.Api.ViewModels.Organisations
+{
+    public class RelatedOrganisationModel
+    {
+        public Guid OrganisationId { get; set; }
+
+        public string Name { get; set; }
+
+        public string OdsCode { get; set; }
+    }
+}
