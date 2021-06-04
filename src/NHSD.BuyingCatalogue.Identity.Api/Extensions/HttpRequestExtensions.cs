b3dab5c4f4f27588abc5bbc97200118c51cf2042
Kiran Singh <kiran.singh@mastek.com>
2021-06-04 08:26:59 +0100

    Bespoke Privacy and Cookie Banner
    Minor refactor
    
    Story: AB#7250
    Task: AB#14970

--- src/NHSD.BuyingCatalogue.Identity.Api/Extensions/HttpRequestExtensions.cs
+++ src/NHSD.BuyingCatalogue.Identity.Api/Extensions/HttpRequestExtensions.cs
@@ -8,23 +8,14 @@ namespace NHSD.BuyingCatalogue.Identity.Api.Extensions
     {
         public static bool ShowCookieConsent(this HttpRequest httpRequest, DateTime? buyingCatalogueCookiePolicyDate)
         {
-            if (httpRequest is null)
-                return false;
-
-            if (!httpRequest.Cookies.TryGetValue(Cookies.BuyingCatalogueConsent, out var consentCookieValue))
-                return true;
-
-            return buyingCatalogueCookiePolicyDate.HasValue
-                && buyingCatalogueCookiePolicyDate.Value < DateTime.Now
-                && DateTime.TryParse(consentCookieValue, out var cookieCreationDate)
-                && cookieCreationDate < buyingCatalogueCookiePolicyDate.Value;
-
-            // return (!httpRequest.Cookies.ContainsKey(Cookies.BuyingCatalogueConsent)
-            //     || (buyingCatalogueCookiePolicyDate.HasValue
-            //         && buyingCatalogueCookiePolicyDate.Value < DateTime.Now
-            //         && httpRequest.Cookies.TryGetValue(Cookies.BuyingCatalogueConsent, out var buyingConsentCookie)
-            //         && DateTime.TryParse(buyingConsentCookie, out var cookieCreationDate)
-            //         && cookieCreationDate < buyingCatalogueCookiePolicyDate));
+            return httpRequest is not null &&
+                (!httpRequest.Cookies.TryGetValue(
+                        Cookies.BuyingCatalogueConsent,
+                        out var consentCookieValue) ||
+                    (buyingCatalogueCookiePolicyDate.HasValue
+                        && buyingCatalogueCookiePolicyDate.Value < DateTime.Now
+                        && DateTime.TryParse(consentCookieValue, out var cookieCreationDate)
+                        && cookieCreationDate < buyingCatalogueCookiePolicyDate.Value));
         }
     }
 }
